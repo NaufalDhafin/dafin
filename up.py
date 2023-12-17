f9447a31929875bee8a886f52e65b35a091f97ba
NaufalDhafin <naufal280905@gmail.com>
2023-12-17 19:39:28 +0700

    pertama

--- up.py
+++ up.py
@@ -1,9 +1,8 @@
 import os
 
 name = str(input("Beri nama untuk mengupload: "))
-print("\n Menyiapkan Folder/File \n")
 os.system("git add .")
 os.system(f'git commit -m "{name}"')
-print("\n Mengupload Folder/File \n")
 os.system("git push")
+
 print("\n Program Selesai, Terimakasih!. \n")
